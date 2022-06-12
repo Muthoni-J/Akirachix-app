63f544974f884b0d248424b480e5279a379689bd
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-06-12 12:22:56 +0300

    first commit

---
+++ app/src/main/java/dev/joanmuthoni/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package dev.joanmuthoni.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnsignup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnsignup  = findViewById(R.id.btnsignup );
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void openNewActivity(){
+
+    }
+}
+
+
